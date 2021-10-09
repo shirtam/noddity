3d27c18b07fa9055d55ca2bfbcb1f89a38bcab1e
Shir <shirtam7@gmail.com>
2021-10-10 02:09:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 noddityConfig = {
-	title: 'Noddity',
+	title: 'CVT - Cloud Vulnerabilities Tracking',
 	noddityRoot: window.location.pathname + 'content/',
 	editLink: 'https://github.com/TehShrike/noddity/edit/gh-pages/content/',
-	logo: 'logo.svg',
+	logo: 'logo.png',
 	errorPage: '404.md',
 	pathPrefix: '#!/',
 	pagePathPrefix: 'post/',
